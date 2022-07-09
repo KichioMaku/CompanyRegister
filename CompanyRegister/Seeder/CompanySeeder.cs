932fc2388d965365cf298eef23b731784a330b87
Jeremiasz Charewicz <jeremiasz.charewicz@microsoft.wsei.edu.pl>
2022-07-10 00:59:11 +0200

    Added new companies

--- CompanyRegister/Seeder/CompanySeeder.cs
+++ CompanyRegister/Seeder/CompanySeeder.cs
@@ -45,9 +45,9 @@ namespace CompanyRegister.Seeder
 							new Person { FirstName = "Janusz", LastName = "Korwin-Mikke", Description = "Członek Zarządu"},
 							new Person { FirstName = "Andrzej", LastName = "Nochal", Description = "Członek Zarządu"},
 							new Person { FirstName = "Mirosław", LastName = "Ciętynochal", Description = "Członek Zarządu"},
-							new Person { FirstName = "Jarosław", LastName = "Michas", Description = "Prokura"},
+							new Person { FirstName = "Jarosław", LastName = "Michas", Description = "Wspólnik"},
 						},
-						Address = new Address { City = "Kraków", Street = "Polna 12", PostalCode = "32-195"}
+						Address = new Address { City = "Cracow", Street = "Polna 12", PostalCode = "32-195"}
 				 },
 
 				new Company
@@ -64,7 +64,7 @@ namespace CompanyRegister.Seeder
 							new Person { FirstName = "Tomasz", LastName = "Pindol", Description = "Członek Zarządu"},
 							new Person { FirstName = "Damian", LastName = "Studnia", Description = "Członek Zarządu"},
 						},
-						Address = new Address { City = "Warszawa", Street = "Fabryczna 23", PostalCode = "30-421"}
+						Address = new Address { City = "Warsaw", Street = "Fabryczna 23", PostalCode = "30-421"}
 				 },
 
 				new Company
@@ -98,7 +98,7 @@ namespace CompanyRegister.Seeder
 							new Person { FirstName = "Mieszko", LastName = "Górski", Description = "Członek Zarządu"},
 							new Person { FirstName = "Oskar", LastName = "Jankowski", Description = "Wspólnik"},
 						},
-						Address = new Address { City = "Warszawa", Street = "Żółta 15", PostalCode = "30-329"}
+						Address = new Address { City = "Warsaw", Street = "Żółta 15", PostalCode = "30-329"}
 				 },
 
 				new Company
@@ -113,15 +113,15 @@ namespace CompanyRegister.Seeder
 						Persons = new List<Person>
 						{
 							new Person { FirstName = "Jędrzej", LastName = "Rutkowski", Description = "Wspólnik"},
-							new Person { FirstName = "Piotr", LastName = "Kowalczyk", Description = "Wspólnik"},
+							new Person { FirstName = "Piotr", LastName = "Kowalczyk", Description = "Członek Zarządu"},
 						},
-						Address = new Address { City = "Warszawa", Street = "Czerwona 69", PostalCode = "30-312"}
+						Address = new Address { City = "Warsaw", Street = "Czerwona 69", PostalCode = "30-312"}
 				 },
 
 				new Company
 				{
 						Name = "Markexshare",
-						Description = "Markexshare is company that specializes in the stock market",
+						Description = "Markexshare is a company that specializes in the stock market",
 						Nip = "9623824797",
 						Krs = "0000347221",
 						Regon = "366353435",
@@ -133,88 +133,79 @@ namespace CompanyRegister.Seeder
 							new Person { FirstName = "Bogumił", LastName = "Makowski", Description = "Członek Zarządu"},
 						},
 						Address = new Address { City = "Opole", Street = "Opolska 14", PostalCode = "24-120"}
-				}
-				// },
+				},
 
-				//new Company
-				//{
-				//		Name = "Med-Pharm",
-				//		Description = "Med-Pharm is an international company that specializes in the manufacture of pharmaceutical products",
-				//		Nip = "6312516302",
-				//		Krs = "0000374328",
-				//		Regon = "240594048",
-				//		ContactEmail = "contact@medpharm.com",
-				//		ContactNumber = "256231945",
-				//		Persons = new List<Person>
-				//		{
-				//			new Person { FirstName = "Janusz", LastName = "Korwin-Mikke", Description = "Członek Zarządu"},
-				//			new Person { FirstName = "Andrzej", LastName = "Nochal", Description = "Członek Zarządu"},
-				//			new Person { FirstName = "Mirosław", LastName = "Ciętynochal", Description = "Główny Analityk"},
-				//			new Person { FirstName = "Jarosław", LastName = "Michas", Description = "Programista"},
-				//			new Person { FirstName = "Jarosław", LastName = "Michas", Description = "Programista"},
-				//		},
-				//		Address = new Address { City = "Kraków", Street = "Polna 12", PostalCode = "32-195"}
-				// },
+				new Company
+				{
+						Name = "GreenLife",
+						Description = "GreenLife is a company that produces ECO friendly kitchen tools",
+						Nip = "3862353865",
+						Krs = "0000217828",
+						Regon = "774933835",
+						ContactEmail = "contact@greenlife.com",
+						ContactNumber = "524579939",
+						Persons = new List<Person>
+						{
+							new Person { FirstName = "Konstanty", LastName = "Lis", Description = "Członek Zarządu"},
+							new Person { FirstName = "Eryk", LastName = "Baranowski", Description = "Członek Zarządu"},
+							new Person { FirstName = "Diego", LastName = "Sawicki", Description = "Członek Zarządu"},
+							new Person { FirstName = "Adam", LastName = "Bąk", Description = "Wspólnik"},
+							new Person { FirstName = "Amadeusz", LastName = "Stępień", Description = "Wspólnik"},
+						},
+						Address = new Address { City = "Szczecin", Street = "Osiedle Złote 144", PostalCode = "12-124"}
+				 },
 
-				//new Company
-				//{
-				//		Name = "Med-Pharm",
-				//		Description = "Med-Pharm is an international company that specializes in the manufacture of pharmaceutical products",
-				//		Nip = "6312516302",
-				//		Krs = "0000374328",
-				//		Regon = "240594048",
-				//		ContactEmail = "contact@medpharm.com",
-				//		ContactNumber = "256231945",
-				//		Persons = new List<Person>
-				//		{
-				//			new Person { FirstName = "Janusz", LastName = "Korwin-Mikke", Description = "Członek Zarządu"},
-				//			new Person { FirstName = "Andrzej", LastName = "Nochal", Description = "Członek Zarządu"},
-				//			new Person { FirstName = "Mirosław", LastName = "Ciętynochal", Description = "Główny Analityk"},
-				//			new Person { FirstName = "Jarosław", LastName = "Michas", Description = "Programista"},
-				//			new Person { FirstName = "Jarosław", LastName = "Michas", Description = "Programista"},
-				//		},
-				//		Address = new Address { City = "Kraków", Street = "Polna 12", PostalCode = "32-195"}
-				// },
+				new Company
+				{
+						Name = "Farmapol",
+						Description = "Farmapol is a company which specializes in production of carpets",
+						Nip = "9937727875",
+						Krs = "0000871322",
+						Regon = "274374872",
+						ContactEmail = "kontakt@farmapol.com.pl",
+						ContactNumber = "976533843",
+						Persons = new List<Person>
+						{
+							new Person { FirstName = "Lucjan", LastName = "Sikora", Description = "Członek Zarządu"},
+							new Person { FirstName = "Alojzy", LastName = "Kołodziej", Description = "Członek Zarządu"},
+						},
+						Address = new Address { City = "Grudziądz", Street = "Szczytowa 3", PostalCode = "15-463"}
+				 },
 
-				//new Company
-				//{
-				//		Name = "Med-Pharm",
-				//		Description = "Med-Pharm is an international company that specializes in the manufacture of pharmaceutical products",
-				//		Nip = "6312516302",
-				//		Krs = "0000374328",
-				//		Regon = "240594048",
-				//		ContactEmail = "contact@medpharm.com",
-				//		ContactNumber = "256231945",
-				//		Persons = new List<Person>
-				//		{
-				//			new Person { FirstName = "Janusz", LastName = "Korwin-Mikke", Description = "Członek Zarządu"},
-				//			new Person { FirstName = "Andrzej", LastName = "Nochal", Description = "Członek Zarządu"},
-				//			new Person { FirstName = "Mirosław", LastName = "Ciętynochal", Description = "Główny Analityk"},
-				//			new Person { FirstName = "Jarosław", LastName = "Michas", Description = "Programista"},
-				//			new Person { FirstName = "Jarosław", LastName = "Michas", Description = "Programista"},
-				//		},
-				//		Address = new Address { City = "Kraków", Street = "Polna 12", PostalCode = "32-195"}
-				// },
+				new Company
+				{
+						Name = "Crem-atory",
+						Description = "Crem-atory is a company that specializes in the crematorial activities",
+						Nip = "9285536955",
+						Krs = "0000128259",
+						Regon = "483898564",
+						ContactEmail = "kontakt@crematory.com.pl",
+						ContactNumber = "494394824",
+						Persons = new List<Person>
+						{
+							new Person { FirstName = "Krzysztof", LastName = "Kononowicz", Description = "Prezes"},
+							new Person { FirstName = "Jerzy", LastName = "Popiołek", Description = "Członek Zarządu"},
+						},
+						Address = new Address { City = "Białystok", Street = "Szkolna 17", PostalCode = "15-640"}
+				 },
 
-				//new Company
-				//{
-				//		Name = "Med-Pharm",
-				//		Description = "Med-Pharm is an international company that specializes in the manufacture of pharmaceutical products",
-				//		Nip = "6312516302",
-				//		Krs = "0000374328",
-				//		Regon = "240594048",
-				//		ContactEmail = "contact@medpharm.com",
-				//		ContactNumber = "256231945",
-				//		Persons = new List<Person>
-				//		{
-				//			new Person { FirstName = "Janusz", LastName = "Korwin-Mikke", Description = "Członek Zarządu"},
-				//			new Person { FirstName = "Andrzej", LastName = "Nochal", Description = "Członek Zarządu"},
-				//			new Person { FirstName = "Mirosław", LastName = "Ciętynochal", Description = "Główny Analityk"},
-				//			new Person { FirstName = "Jarosław", LastName = "Michas", Description = "Programista"},
-				//			new Person { FirstName = "Jarosław", LastName = "Michas", Description = "Programista"},
-				//		},
-				//		Address = new Address { City = "Kraków", Street = "Polna 12", PostalCode = "32-195"}
-				// }
+				new Company
+				{
+						Name = "ExtraPonton",
+						Description = "ExtraPonton is a company that specializes in a production of inflatables",
+						Nip = "8358742772",
+						Krs = "0000853877",
+						Regon = "677646655",
+						ContactEmail = "kontakt@extraponton.com.pl",
+						ContactNumber = "247786854",
+						Persons = new List<Person>
+						{
+							new Person { FirstName = "Michał", LastName = "Włodarczyk", Description = "Członek Zarządu"},
+							new Person { FirstName = "Kacper", LastName = "Błaszczyk", Description = "Członek Zarządu"},
+							new Person { FirstName = "Ludwik", LastName = "Mazur", Description = "Prezes"},
+						},
+						Address = new Address { City = "Gdańsk", Street = "Morska 235", PostalCode = "13-246"}
+				 }
 
 			};
 			return companies;
