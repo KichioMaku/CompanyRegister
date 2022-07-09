89ae6f0c69acfb85d11f2626d8b0132390fb9ad4
Jeremiasz Charewicz <jeremiasz.charewicz@microsoft.wsei.edu.pl>
2022-07-09 21:14:38 +0200

    CompanyDbContext - basic data validation added

--- CompanyRegister/Entities/CompanyDbContext.cs
+++ CompanyRegister/Entities/CompanyDbContext.cs
@@ -7,5 +7,37 @@ namespace CompanyRegister.Entities
 		public DbSet<Company> Companies { get; set; }
 		public DbSet<Person> Persons { get; set; }
 		public DbSet<Address> Address { get; set; }
+
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Company>()
+				.Property(x => x.Name)
+                .IsRequired()
+                .HasMaxLength(50); 
+
+			modelBuilder.Entity<Company>()
+				.Property(x => x.Description)
+                .IsRequired();
+
+            modelBuilder.Entity<Address>()
+                .Property(x => x.City)
+                .IsRequired();
+
+            modelBuilder.Entity<Address>()
+                .Property(x => x.Street)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Person>()
+                .Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            modelBuilder.Entity<Person>()
+                .Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(50); 
+        }
 	}
 }
