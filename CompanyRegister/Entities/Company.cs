bf6ed6d35901dea33d682f3c1123b9926962684e
Jeremiasz Charewicz <jeremiasz.charewicz@microsoft.wsei.edu.pl>
2022-07-09 20:51:52 +0200

    Add Company Entity

---
+++ CompanyRegister/Entities/Company.cs
@@ -0,0 +1,17 @@
+﻿namespace CompanyRegister.Entities
+{
+	public class Company
+	{
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Nip { get; set; }
+        public string Krs { get; set; }
+        public string Regon { get; set; }
+        public string ContactEmail { get; set; }
+        public string ContactNumber { get; set; }
+        public int AddressId { get; set; }
+        //public virtual Address Address { get; set; }
+        //public virtual List<Person> Persons { get; set; }
+    }
+}
