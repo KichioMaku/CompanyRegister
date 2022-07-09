24225419851a31e39b80bfba88a65588e1750d86
Jeremiasz Charewicz <jeremiasz.charewicz@microsoft.wsei.edu.pl>
2022-07-10 00:31:45 +0200

    Update PersonService, PersonController

--- CompanyRegister/Controllers/PersonController.cs
+++ CompanyRegister/Controllers/PersonController.cs
@@ -1,4 +1,5 @@
-﻿using CompanyRegister.Services;
+﻿using CompanyRegister.Models;
+using CompanyRegister.Services;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
