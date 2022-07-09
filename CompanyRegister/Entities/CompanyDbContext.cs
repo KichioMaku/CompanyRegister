ca56beec632526940c34c0254b1a9bb43140282a
Mateusz Charewicz <mateusz.charewicz@microsoft.wsei.edu.pl>
2022-07-09 21:02:51 +0200

    Add CompanyDbContext

---
+++ CompanyRegister/Entities/CompanyDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CompanyRegister.Entities
+{
+	public class CompanyDbContext : DbContext
+	{
+		public DbSet<Company> Companies { get; set; }
+		public DbSet<Person> Persons { get; set; }
+		public DbSet<Address> Address { get; set; }
+	}
+}
