ff9073543dcee462c1c289b3cf0a6faf6b74a71c
Jeremiasz Charewicz <jeremiasz.charewicz@microsoft.wsei.edu.pl>
2022-07-10 00:30:04 +0200

    Add PersonController, PersonDto, CreatePersonDto

---
+++ CompanyRegister/Controllers/PersonController.cs
@@ -0,0 +1,49 @@
+﻿using CompanyRegister.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace CompanyRegister.Controllers
+{
+    [ApiController]
+    [Route("api/company/{companyId}/person")]
+    public class PersonController : ControllerBase
+    {
+        private readonly IPersonService _personService;
+
+        public PersonController(IPersonService personService)
+        {
+            _personService = personService;
+        }
+
+        [HttpDelete]
+        public ActionResult Delete([FromRoute] int companyId)
+        {
+            _personService.RemoveAll(companyId);
+            return NoContent();
+        }
+
+        [HttpPost]
+        public ActionResult Post([FromRoute] int companyId, [FromBody] CreatePersonDto dto)
+        {
+            var newPersonId = _personService.Create(companyId, dto);
+
+            return Created($"api/company/{companyId}/person/{newPersonId}", null);
+        }
+
+        [HttpGet("{personId}")]
+        public ActionResult<PersonDto> Get([FromRoute] int companyId,
+            [FromRoute] int personId)
+        {
+            PersonDto person = _personService.GetById(companyId, personId);
+            return Ok(person);
+        }
+
+        [HttpGet]
+        public ActionResult<List<PersonDto>> Get([FromRoute] int companyId)
+        {
+            var result = _personService.GetAll(companyId);
+            return Ok(result);
+        }
+    }
+}
---
+++ CompanyRegister/Models/CreatePersonDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CompanyRegister.Models
+{
+	public class CreatePersonDto
+	{
+        [Required]
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
